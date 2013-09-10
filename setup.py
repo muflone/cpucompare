99630f74cddb0dbbc6a0cd090e5eaffd92b344eb
Muflone <muflone@vbsimple.net>
2013-09-10 12:03:05 +0200

    Added setup.py distutils script to handle the installation

---
+++ setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python2
+##
+#     Project: CPUCompare
+# Description: A GTK+ application to make comparisons between CPU models
+#      Author: Fabio Castelli <muflone@vbsimple.net>
+#   Copyright: 2013 Fabio Castelli
+#     License: GPL-2+
+#  This program is free software; you can redistribute it and/or modify it
+#  under the terms of the GNU General Public License as published by the Free
+#  Software Foundation; either version 2 of the License, or (at your option)
+#  any later version.
+#
+#  This program is distributed in the hope that it will be useful, but WITHOUT
+#  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+#  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+#  more details.
+#  You should have received a copy of the GNU General Public License along
+#  with this program; if not, write to the Free Software Foundation, Inc.,
+#  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+##
+
+from distutils.core import setup
+from distutils.command.install_scripts import install_scripts
+
+from glob import glob
+import shutil
+from cpucompare.constants import *
+
+class rename_python_scripts(install_scripts):
+  def run(self):
+    install_scripts.run(self)
+    for script in self.get_outputs():
+      if script.endswith(".py"):
+        shutil.move(script, script[:-3])
+
+setup(
+  name=APP_NAME,
+  version=APP_VERSION,
+  description=APP_DESCRIPTION,
+  author=APP_AUTHOR,
+  author_email=APP_AUTHOR_EMAIL,
+  maintainer=APP_AUTHOR,
+  maintainer_email=APP_AUTHOR_EMAIL,
+  url=APP_URL,
+  license='GPL v2',
+  scripts=['cpucompare.py'],
+  packages=['cpucompare'],
+  data_files=[
+    ('share/cpucompare/data', glob('data/*')),
+    ('share/cpucompare/ui', glob('ui/*.glade'))
+  ],
+  cmdclass = {
+    'install_scripts': rename_python_scripts
+  }
+)
