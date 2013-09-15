c036cfe3ff244a2f8db1adc396c1aba79a8dca77
Muflone <webreg@vbsimple.net>
2013-09-15 18:15:32 +0200

    Database connection opened to allow access from different threads

--- cpucompare/database.py
+++ cpucompare/database.py
@@ -29,7 +29,7 @@ class SQLite3Connection(object):
 
   def open(self, sDatabase):
     # Open the database connection
-    self.connection = sqlite3.connect(sDatabase)
+    self.connection = sqlite3.connect(sDatabase, check_same_thread = False)
     self.connection.row_factory = sqlite3.Row
 
   def close(self):
