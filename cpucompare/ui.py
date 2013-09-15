95ad8be598af3d3b86708d081ef69bc882248b72
Muflone <webreg@vbsimple.net>
2013-09-15 18:08:55 +0200

    Set about dialog transient for main window

--- cpucompare/ui.py
+++ cpucompare/ui.py
@@ -206,6 +206,7 @@ class CPUCompareUI(Gtk.Application):
     dlgAbout.set_website(APP_URL)
     dlgAbout.set_copyright(APP_COPYRIGHT)
     dlgAbout.set_authors(['%s <%s>' % (APP_AUTHOR, APP_AUTHOR_EMAIL)]),
+    dlgAbout.set_transient_for(self.winMain)
     dlgAbout.run()
     dlgAbout.destroy()
 
