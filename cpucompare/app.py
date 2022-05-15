6bc0610f0da7242120f94df59fa4e99c93221d75
Muflone <webreg@muflone.com>
2022-05-15 03:27:30 +0200

    Dropped AppMenu

--- cpucompare/app.py
+++ cpucompare/app.py
@@ -19,11 +19,8 @@
 ##
 
 from gi.repository import Gtk
-from gi.repository import Gio
 
 from cpucompare.constants import APP_ID
-from cpucompare.gtkbuilder_loader import GtkBuilderLoader
-
 from cpucompare.ui.main import UIMain
 
 
@@ -38,36 +35,7 @@ class Application(Gtk.Application):
     def startup(self, application):
         """Configure the application during the startup"""
         self.ui = UIMain(self)
-        # Add the about action to the app menu
-        action = Gio.SimpleAction(name="about")
-        action.connect("activate", self.on_app_about_activate)
-        self.add_action(action)
-        # Add the shortcut action to the app menu
-        # only for GTK+ 3.20.0 and higher
-        if not Gtk.check_version(3, 20, 0):
-            action = Gio.SimpleAction(name="shortcuts")
-            action.connect("activate", self.on_app_shortcuts_activate)
-            self.add_action(action)
-        # Add the quit action to the app menu
-        action = Gio.SimpleAction(name="quit")
-        action.connect("activate", self.on_app_quit_activate)
-        self.add_action(action)
-        # Add the app menu
-        builder_appmenu = GtkBuilderLoader('appmenu.ui')
-        self.set_app_menu(builder_appmenu.app_menu)
 
     def activate(self, application):
         """Execute the application"""
         self.ui.run()
-
-    def on_app_about_activate(self, action, data):
-        """Show the about dialog from the app menu"""
-        self.ui.on_action_application_about_activate(action)
-
-    def on_app_shortcuts_activate(self, action, data):
-        """Show the shortcuts dialog from the app menu"""
-        self.ui.on_action_application_shortcuts_activate(action)
-
-    def on_app_quit_activate(self, action, data):
-        """Quit the application from the app menu"""
-        self.ui.on_action_application_quit_activate(action)
